cf6d6a698117f84e5ed1a64526ba8a67446cc4c9
joelwanjohi <joelwanjohi207@gmail.com>
2024-10-15 22:23:53 +0300

    homescreen

---
+++ lib/screens/add_todo_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../services/todo_service.dart';
+import '../models/todo.dart';
+
+class AddTodoScreen extends StatelessWidget {
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _bodyController = TextEditingController();
+  final TodoService _todoService = TodoService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Todo'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(labelText: 'Title'),
+            ),
+            TextField(
+              controller: _bodyController,
+              decoration: InputDecoration(labelText: 'Body'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () async {
+                final todo = Todo(
+                  id: '', // The ID will be set by the server
+                  title: _titleController.text,
+                  body: _bodyController.text,
+                  completed: false,
+                );
+                try {
+                  await _todoService.addTodo(todo);
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Todo added successfully')),
+                  );
+                  Navigator.pop(context);
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Failed to add todo: ${e.toString()}')),
+                  );
+                }
+              },
+              child: Text('Add Todo'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/services/todo_service.dart
@@ -0,0 +1,48 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../models/todo.dart';
+
+class TodoService {
+  final String baseUrl = 'http://10.0.2.2:3000/todos'; // Use your API base URL
+
+  Future<List<Todo>> getTodos() async {
+    final response = await http.get(Uri.parse(baseUrl));
+    if (response.statusCode == 200) {
+      List<dynamic> body = jsonDecode(response.body);
+      return body.map((json) => Todo.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load todos');
+    }
+  }
+
+  Future<Todo> addTodo(Todo todo) async {
+    final response = await http.post(
+      Uri.parse(baseUrl),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(todo.toJson()),
+    );
+    if (response.statusCode == 201) {
+      return Todo.fromJson(jsonDecode(response.body));
+    } else {
+      throw Exception('Failed to create todo');
+    }
+  }
+
+  Future<void> updateTodo(String id, Todo todo) async {
+    final response = await http.put(
+      Uri.parse('$baseUrl/$id'),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(todo.toJson()),
+    );
+    if (response.statusCode != 200) {
+      throw Exception('Failed to update todo');
+    }
+  }
+
+  Future<void> deleteTodo(String id) async {
+    final response = await http.delete(Uri.parse('$baseUrl/$id'));
+    if (response.statusCode != 200) {
+      throw Exception('Failed to delete todo');
+    }
+  }
+}
