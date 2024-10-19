614f9febebabfe89873616604d11010700515cad
joelwanjohi <joelwanjohi207@gmail.com>
2024-10-19 23:47:36 +0300

    nmmsn

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,3 +1,10 @@
 import 'package:flutter/material.dart';
 
 
+Signal<bool> sIsDark = signal<bool>(false);
+
+Signal<bool> sIsGreen = signal<bool>(true);
+
+Computed<FlexScheme> cFlexScheme =  computed(() {
+  return sIsGreen.value ? FlexScheme.money : FlexScheme.espresso;
+});
